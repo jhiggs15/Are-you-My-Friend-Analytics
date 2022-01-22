52b56ba7f5a71f0d0e4bcf094592b6479d735c97
Gus Teran <ateran@wpi.edu>
2022-01-22 17:23:07 -0500

    Add access csv method stubs
    Create this so I remember what to work on later

---
+++ AccessData.py
@@ -0,0 +1,15 @@
+# AccessId: unique sequential number (integer) from 1 to 10,000,000
+# ByWho: References the Id of the person who has accessed the Facebook page
+# WhatPage: References the Id of the page that was accessed
+# TypeOfAccess: text  of  characters  of  length  between  20  and  50  explaining  if  just
+# viewed, left a note, added a friendship, etc.
+# AccessTime: random number between 1 and 1,000,000 (or epoch time)
+
+def accessCVS():
+    # create access CSV
+
+def generateAccessRow():
+    # generate each row of the access CSV
+
+if __name__ == '__main__':
+    accessCVS()
